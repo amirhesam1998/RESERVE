af8199a3e9c15ec8259cb17aae40947f31482f39
Arian Mollai <ar.mollaie82@gmail.com>
2026-08-16 21:37:18 +0330

    Lohin fixed

--- app/Http/Middleware/EnsureUserIsGuest.php
+++ app/Http/Middleware/EnsureUserIsGuest.php
@@ -4,8 +4,10 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class EnsureUserIsGuest
 {
@@ -18,7 +20,8 @@ class EnsureUserIsGuest
     {
         $token = $request->cookie('access_token');
         if ($token) {
-            return Redirect()->route('/');
+            $user = JWTAuth::setToken($token)->authenticate();
+            return redirect()->route('users.show', $user->id);
         }
 
         return $next($request);
--- routes/Web/users.php
+++ routes/Web/users.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\Web\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    return redirect()->route('login');
 })->name('/');
 
 // -------------------- USER CRUD -------------------------------
